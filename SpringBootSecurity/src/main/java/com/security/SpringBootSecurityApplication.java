0220411a95a1a586c936a2ad847c5329b8c1e97b
Vitor Pereira <vitorpereiraresende26@gmail.com>
2023-04-14 18:00:51 -0300

    Roles and Admin User loaded

--- SpringBootSecurity/src/main/java/com/security/SpringBootSecurityApplication.java
+++ SpringBootSecurity/src/main/java/com/security/SpringBootSecurityApplication.java
@@ -1,13 +1,40 @@
 package com.security;
 
+import com.security.model.ApplicationUser;
+import com.security.model.Role;
+import com.security.repository.RoleRepository;
+import com.security.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class SpringBootSecurityApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootSecurityApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder encoder){
+		return args -> {
+			if(roleRepository.findByAuthority("ADMIN").isPresent()) return;
+
+			Role adminRole = roleRepository.save(new Role("ADMIN"));
+			roleRepository.save(new Role("USER"));
+
+			Set<Role> roles = new HashSet<>();
+			roles.add(adminRole);
+
+			ApplicationUser admin = new ApplicationUser(1, "admin", encoder.encode("password"), roles);
+
+			userRepository.save(admin);
+		};
+	}
 }
---
+++ SpringBootSecurity/src/main/java/com/security/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.security.repository;
+
+import com.security.model.ApplicationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<ApplicationUser, Integer> {
+
+    Optional<ApplicationUser> findByUsername(String username);
+}
